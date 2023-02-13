9f9bf997ae668769c185b4bc6e2f876be4a4f340
Rohan Puthukudy <rohan@gleebee.com>
2023-02-12 23:24:13 -0800

    enabled auth workflow

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const { MongoClient } = require('mongodb');
 const port = 3000;
+const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.0';
+const client = new MongoClient(uri);
+
+let db;
+
+async function connect() {
+  // Connect the client to the server (optional starting in v4.7)
+  await client.connect();
+  // Establish and verify connection
+  db = client.db('test-devs');
+  console.log("Connected successfully to server");
+}
+connect().catch(console.dir);
 
 const sess = {
   secret: 'this should be secret and random',
   resave: true,
   saveUninitialized: true,
 };
-
-let user = "";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,10 +32,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
-app.post('/login', (req, res) => {
-  user = req.body.username;
-  req.session.user = user;
-  res.sendFile(__dirname + '/home.html');
+app.post('/login', async (req, res) => {
+  const user = req.body.username;
+  const pass = req.body.password;
+  const query = {username: user, password: pass};
+  const result = await db.collection('users').findOne(query);
+  if (result != null) {
+    req.session.user = user;
+    res.sendFile(__dirname + '/home.html');
+  } else {
+    console.log('No such user!');
+    res.sendStatus(404);
+  }
 });
 
 app.listen(port, () => {
