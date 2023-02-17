821c0ec7c0fa6fa5fa100ef8b99da49422cf30aa
Rohan Puthukudy <rohan@gleebee.com>
2023-02-16 16:52:55 -0800

    commented index.js

--- index.js
+++ index.js
@@ -4,34 +4,40 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const { MongoClient } = require('mongodb');
 const port = 3000;
-const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.0';
-const client = new MongoClient(uri);
-
-let db;
-
-async function connect() {
-  // Connect the client to the server (optional starting in v4.7)
-  await client.connect();
-  // Establish and verify connection
-  db = client.db('test-devs');
-  console.log("Connected successfully to server");
-}
-connect().catch(console.dir);
-
 const sess = {
   secret: 'this should be secret and random',
   resave: true,
   saveUninitialized: true,
 };
 
+// Set this equal to your MongoDB URI
+const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.0';
+const client = new MongoClient(uri);
+
+// Change this if you're using a different name
+const dbName = 'testDevs'; 
+
+// This global variable is our database connection and is accessible everywhere
+let db;
+async function connect() {
+    await client.connect();
+    db = client.db(dbName);
+    console.log("Connected successfully to MongoDB server");
+}
+connect();
+
+// Sets up the Express server
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session(sess));
 
+// Server's base route
 app.get('/', (req, res) => {
+  // Displays our login.html file at the root route
   res.sendFile(__dirname + '/login.html');
 });
 
+// Exc. 3.1: Handle POST request to /login route
 app.post('/login', async (req, res) => {
   const user = req.body.username;
   const pass = req.body.password;
@@ -46,6 +52,7 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// Runs the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 });
