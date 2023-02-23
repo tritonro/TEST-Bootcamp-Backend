d0458b6a52e4f766054d77c6f593c2bc4bfcf515
Rohan Puthukudy <rohan@gleebee.com>
2023-02-23 11:16:27 -0800

    removed solutions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const sess = {
 };
 
 // Set this equal to your MongoDB URI
-const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.0';
+const uri = '';
 const client = new MongoClient(uri);
 
 // Change this if you're using a different name
@@ -35,32 +35,17 @@ app.use(express.static(__dirname));
 // Server's base route
 app.get('/', (req, res) => {
   // Displays our login.html file at the root route
-  if(req.session.loggedIn) 
-    res.sendFile(__dirname + '/home.html');
-  else
-    res.sendFile(__dirname + '/login.html');
+  res.sendFile(__dirname + '/login.html');
 });
 
 // Exc. 3.1: Handle POST request to /login route
 app.post('/login', async (req, res) => {
-  const user = req.body.username;
-  const pass = req.body.password;
-  const query = {username: user, password: pass};
-  const result = await db.collection('users').findOne(query);
-  if (result != null) {
-    req.session.user = user;
-    req.session.loggedIn = true;
-    res.redirect('/');
-  } else {
-    req.session.loggedIn = false;
-    console.log('No such user!');
-    res.sendStatus(404);
-  }
+  // TODO
 });
 
 // Exc. 3.2: Render the username
 app.get('/user', (req, res) => {
-  res.json(req.session.user);
+  // TODO
 });
 
 // Runs the server
