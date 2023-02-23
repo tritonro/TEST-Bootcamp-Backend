b5f934dd7eff35f5910cd15bc2bef4a458d3de7d
Rohan Puthukudy <rohan@gleebee.com>
2023-02-23 11:23:59 -0800

    deleted solution in message.js

--- message.js
+++
@@ -1,8 +0,0 @@
-const msgElem = document.getElementById('welcome');
-
-fetch('http://localhost:3000/user')
-  .then( r => r.json() )
-  .then( (r) => {
-    msgElem.textContent = `Welcome ${r}`;
-  });
-
